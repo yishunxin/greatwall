d21c0c70bf244dba1760cfa4da5d4a3ef6d2c29c
yishunxin <991007889@qq.com>
2019-07-01 18:40:07 +0800

    update

--- gs/baihe/__init__.py
+++ gs/baihe/__init__.py
@@ -1,1 +1,11 @@
-# -*- coding:utf-8 -*-
+import json
+import requests
+def post_login():
+    datas = {
+        'phone': 'admin',
+        'password': '123456'
+    }
+
+    resp = requests.post("http://tj.sparkcab.com/api001/login", data = json.dumps(datas))
+    print resp.content
+post_login()
