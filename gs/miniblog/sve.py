3f85ba02e32e5d127391c03ffff8779550785de8
yishunxin <991007889@qq.com>
2020-05-27 18:51:44 +0800

    'update'

--- gs/miniblog/sve.py
+++ gs/miniblog/sve.py
@@ -1,11 +1,50 @@
-from flask import Flask, render_template
 import logging
+
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@127.0.0.1:3306/miniblog?charset=utf8mb4'
+db = SQLAlchemy(app, session_options={'autoflush': False})
+
+
+class ShoppingList(db.Model):
+    __tablename__ = 'shopping_list'
+    list_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Integer)
+
+
+class Item(db.Model):
+    __tablename__ = 'item'
+    item_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    price = db.Column(db.Float)
+    image = db.Column(db.String)
+    url = db.Column(db.String)
+    desc = db.Column(db.String)
+    list_id = db.Column(db.String)
+
+
+class Svc(object):
+    def all_item(self):
+        shoppinglists = db.session.query(ShoppingList).all()
+        items = db.session.query(Item).all()
+        item_map = {}
+        for item in items:
+            if item.list_id not in item_map:
+                item_map[item.list_id] = [item]
+            else:
+                item_map[item.list_id].append(item)
+        for shoppinglist in shoppinglists:
+            shoppinglist.items = item_map.get(shoppinglist.list_id)
+        return shoppinglists
+
 
 @app.route('/')
 def index():
     try:
-        return render_template("index.html")
+        shoppinglists = Svc().all_item()
+        return render_template("index.html", shoppinglists=shoppinglists)
     except Exception as e:
         logging.exception(e)
-        return False
+        return render_template('error.html')
